2c4b1e4317b8a67510d00c934b9e31f655dbe8ba
Daniel Sørensen <danielftw321@gmail.com>
2019-03-07 13:20:01 +0100

    Geomatri

---
+++ OBGeometri/OBGeometri/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBGeometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sqaure sqaure = new Sqaure();
+            Sqaure sqaure2 = new Sqaure();
+
+            sqaure.A = 20;
+            sqaure2.A = 30;
+
+            Console.WriteLine($"Calculate squares\nFirst square");
+
+            sqaure.SquareCalculate();
+
+            Console.WriteLine(sqaure.Result);
+
+
+            Console.WriteLine("Calculating secound square");
+
+            sqaure2.SquareCalculate();
+
+            Console.WriteLine(sqaure2.Result);
+            
+               
+            sqaure = new Sqaure();
+
+            sqaure.A = 24;
+
+            Console.WriteLine("Area calculate");
+
+            sqaure.AreaCaluculate();
+
+            Console.WriteLine(sqaure.Result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OBGeometri/OBGeometri/Sqaure.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBGeometri
+{
+    public class Sqaure
+    {
+        //Attribute
+        private double a;
+        private double result;
+
+
+        //Properties
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+
+        public double Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+            }
+        }
+
+
+
+        //constructor
+        public Sqaure()
+        {
+            
+
+        }
+
+
+
+        //Methods
+        public void SquareCalculate()
+        {
+            this.Result = 4 * this.a;
+            
+        }
+
+        public void AreaCaluculate()
+        {
+
+            this.Result = Math.Pow(a,2);
+
+        }
+
+
+
+    }
+}
