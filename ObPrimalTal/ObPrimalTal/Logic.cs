b4f9463a7774a56a60619df9d587bdb1843991d9
Daniel Sørensen <danielftw321@gmail.com>
2019-03-11 10:52:20 +0100

    PritmalTalDOne

---
+++ ObPrimalTal/ObPrimalTal/Logic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObPrimalTal
+{
+    class Logic
+    {
+        //Attribute
+        Queue<string> names = new Queue<string>();
+        List<string> namelist = new List<string>() {"Dennis","Benny","Bent","Carla","Dom","Karla","René","Zanta" };
+        Random ran = new Random();
+        //Properties
+        public Queue<string> Names
+        {
+            get { return names; }
+            set
+            {
+                names = value;
+            }
+        }
+        //Constructor
+        public Logic()
+        {
+            names.Enqueue("Daniel");
+            names.Enqueue("Peter");
+            names.Enqueue("Bente");
+            names.Enqueue("Julz");
+        }
+        //Methods
+
+        public void DeleteQueueInFakta()
+        {
+            while (Names.Count > 0)
+            {
+                names.Dequeue();
+            }
+        }
+
+        public void AddNames()
+        {
+            while (names.Count < 5)
+            {
+                names.Enqueue(namelist[ran.Next(0,namelist.Count)]);
+            }
+        }
+
+        public string PrintNames()
+        {
+            string temp = "";
+            while (Names.Count > 0)
+            {
+                temp += $"{Names.Dequeue()}\n";
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ ObPrimalTal/ObPrimalTal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObPrimalTal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+
+             
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu Option.\nPress [1] to print name queue.\nPress [2] Add random names.\nPress [3] to delete the hole list.");
+                ConsoleKeyInfo input = Console.ReadKey();
+                Console.Clear();
+                switch (input.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine(logic.PrintNames());
+                        break;
+
+                    case '2':
+                        logic.AddNames();
+                        Console.WriteLine("Names added");
+                        break;
+
+                    case '3':
+                        logic.DeleteQueueInFakta();
+                        Console.WriteLine("Queue deleted");
+                        break;
+
+                    default:
+                        Console.WriteLine("Something went wrong, now we have to do it alllllll over again.............");
+                        break;
+                }
+                Console.WriteLine("\nPress any key to continue");
+                Console.ReadKey();
+            }
+
+
+
+
+
+        }
+    }
+}
